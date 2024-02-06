4e120b3c3d6f3b91f53a1b61a5ce1597d1939704
bragelbytes <joebragel@gmail.com>
2024-02-06 11:41:49 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
             targets: ["BigNumber"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission", branch: "main"),
-        .package(url: "https://github.com/OperatorFoundation/TransmissionData", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable", branch: "4.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftHexTools", branch: "1.2.6"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/TransmissionData", branch: "release"),
     ],
     targets: [
         .target(
